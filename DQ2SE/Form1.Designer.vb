061c0dcc75f2e2151012f0ef52d71f818e4df65b
Althir <fed0r@users.noreply.github.com>
2019-07-12 14:21:25 +0300

    First release
    v1.0 first release.
    Missing features.

---
+++ DQ2SE/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.LoadFileBtn = New System.Windows.Forms.Button()
+        Me.PlayerProfiles = New System.Windows.Forms.ListBox()
+        Me.AddGoldBtn = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.StatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.DifficultyBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PotsButton = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.LoadFileBtn)
+        Me.GroupBox1.Controls.Add(Me.PlayerProfiles)
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(341, 220)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Load data"
+        '
+        'LoadFileBtn
+        '
+        Me.LoadFileBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LoadFileBtn.ForeColor = System.Drawing.Color.Red
+        Me.LoadFileBtn.Location = New System.Drawing.Point(194, 19)
+        Me.LoadFileBtn.Name = "LoadFileBtn"
+        Me.LoadFileBtn.Size = New System.Drawing.Size(128, 43)
+        Me.LoadFileBtn.TabIndex = 4
+        Me.LoadFileBtn.Text = "Load file"
+        Me.LoadFileBtn.UseVisualStyleBackColor = True
+        '
+        'PlayerProfiles
+        '
+        Me.PlayerProfiles.FormattingEnabled = True
+        Me.PlayerProfiles.Location = New System.Drawing.Point(6, 19)
+        Me.PlayerProfiles.Name = "PlayerProfiles"
+        Me.PlayerProfiles.Size = New System.Drawing.Size(170, 43)
+        Me.PlayerProfiles.TabIndex = 3
+        '
+        'AddGoldBtn
+        '
+        Me.AddGoldBtn.Enabled = False
+        Me.AddGoldBtn.Location = New System.Drawing.Point(34, 19)
+        Me.AddGoldBtn.Name = "AddGoldBtn"
+        Me.AddGoldBtn.Size = New System.Drawing.Size(99, 27)
+        Me.AddGoldBtn.TabIndex = 2
+        Me.AddGoldBtn.Text = "Add Gold"
+        Me.ToolTip1.SetToolTip(Me.AddGoldBtn, "Push this button for massive amount of gold!")
+        Me.AddGoldBtn.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.PictureBox1)
+        Me.GroupBox2.Controls.Add(Me.PotsButton)
+        Me.GroupBox2.Controls.Add(Me.GroupBox3)
+        Me.GroupBox2.Controls.Add(Me.AddGoldBtn)
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 80)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(341, 309)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Editor"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StatusLabel})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 392)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(354, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(59, 17)
+        Me.StatusLabel.Text = "Standby..."
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.DQ2SE.My.Resources.Resources.nice1
+        Me.PictureBox1.Location = New System.Drawing.Point(183, 161)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(158, 148)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.DifficultyBtn)
+        Me.GroupBox3.Controls.Add(Me.RadioButton3)
+        Me.GroupBox3.Controls.Add(Me.RadioButton2)
+        Me.GroupBox3.Controls.Add(Me.RadioButton1)
+        Me.GroupBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(183, 9)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(152, 145)
+        Me.GroupBox3.TabIndex = 4
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Difficulty"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Enabled = False
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 46)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(48, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Easy"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Enabled = False
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(6, 70)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Normal"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Enabled = False
+        Me.RadioButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.Location = New System.Drawing.Point(6, 93)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(69, 17)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Hardcore"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'DifficultyBtn
+        '
+        Me.DifficultyBtn.Enabled = False
+        Me.DifficultyBtn.Location = New System.Drawing.Point(6, 116)
+        Me.DifficultyBtn.Name = "DifficultyBtn"
+        Me.DifficultyBtn.Size = New System.Drawing.Size(140, 23)
+        Me.DifficultyBtn.TabIndex = 5
+        Me.DifficultyBtn.Text = "Change"
+        Me.DifficultyBtn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 26)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Too easy or too hard?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Change difficulty!"
+        '
+        'PotsButton
+        '
+        Me.PotsButton.Enabled = False
+        Me.PotsButton.Location = New System.Drawing.Point(34, 55)
+        Me.PotsButton.Name = "PotsButton"
+        Me.PotsButton.Size = New System.Drawing.Size(99, 27)
+        Me.PotsButton.TabIndex = 5
+        Me.PotsButton.Text = "Add Magic Pots"
+        Me.ToolTip1.SetToolTip(Me.PotsButton, "Push this button for massive amount of gold!")
+        Me.PotsButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(354, 414)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "DQ2SE - Dark Quest II Save Editor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents AddGoldBtn As Button
+    Friend WithEvents PlayerProfiles As ListBox
+    Friend WithEvents LoadFileBtn As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents StatusLabel As ToolStripStatusLabel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DifficultyBtn As Button
+    Friend WithEvents PotsButton As Button
+End Class
---
+++ DQ2SE/Form1.vb
@@ -0,0 +1,74 @@
+﻿' Dark Quest II Save Editor
+' Tool by Althir
+' This software is provided free of charge
+Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PlayerProfiles.Items.Clear()
+        Dim grade As New IO.DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Appdata\\LocalLow\Brain Seal Ltd\Dark Quest 2\")
+        Dim diar As IO.FileInfo() = grade.GetFiles("*.bin")
+        Dim dra As IO.FileInfo
+        For Each dra In diar
+            PlayerProfiles.Items.Add(System.IO.Path.GetFileNameWithoutExtension(dra.FullName))
+        Next
+        PlayerProfiles.Items.Remove("gamesettings")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles LoadFileBtn.Click
+        If File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Appdata\\LocalLow\Brain Seal Ltd\Dark Quest 2\" & PlayerProfiles.SelectedItem & ".bin") Then
+            PlayerProfiles.Enabled = False
+            LoadFileBtn.Enabled = False
+            StatusLabel.Text = "Editing a file!"
+            StatusLabel.ForeColor = Color.Green
+            'Enable options after load
+            AddGoldBtn.Enabled = True
+            PotsButton.Enabled = True
+            RadioButton1.Enabled = True
+            RadioButton2.Enabled = True
+            RadioButton3.Enabled = True
+            DifficultyBtn.Enabled = True
+            '--------------------
+            MsgBox("Notice: This tool is a save editor, not memory editor! Please exit from game completely before using this software.", MsgBoxStyle.Information, "DQ2SE")
+        ElseIf PlayerProfiles.SelectedItem = "" Then
+            MsgBox("Notice: You did not select a file!", MsgBoxStyle.Exclamation, "DQ2SE")
+        Else
+            MsgBox("Notice: Unknown error. Please try again!", MsgBoxStyle.Information, "DQ2SE")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles AddGoldBtn.Click
+        Dim fs As New IO.FileStream(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Appdata\\LocalLow\Brain Seal Ltd\Dark Quest 2\" & PlayerProfiles.SelectedItem & ".bin", FileMode.Open, FileAccess.ReadWrite)
+        fs.Position = &H37
+        fs.WriteByte(&H98)
+        fs.Position = &H38
+        fs.WriteByte(&H96)
+        fs.Position = &H39
+        fs.WriteByte(&H80)
+        fs.Close()
+        fs.Dispose()
+        StatusLabel.ForeColor = Color.Red
+        StatusLabel.Text = "GOLD ADDED!"
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        MsgBox("DQ2SE By ALTHiR. This tool is 100% Freeware and was made for my own entertainment. Version 1.0", MsgBoxStyle.Information, "DQ2SE")
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles PotsButton.Click
+        Dim fs As New IO.FileStream(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Appdata\\LocalLow\Brain Seal Ltd\Dark Quest 2\" & PlayerProfiles.SelectedItem & ".bin", FileMode.Open, FileAccess.ReadWrite)
+        fs.Position = &H16D
+        fs.WriteByte(&HFF)
+        fs.Position = &H16E
+        fs.WriteByte(&HFF)
+        fs.Position = &H16F
+        fs.WriteByte(&HFF)
+        fs.Close()
+        fs.Dispose()
+        StatusLabel.ForeColor = Color.Red
+        StatusLabel.Text = "POTS ADDED!"
+    End Sub
+End Class
